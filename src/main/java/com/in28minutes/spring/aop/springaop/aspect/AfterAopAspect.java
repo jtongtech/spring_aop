63cbe404b9d6dfda68ffb4ecc45e03fe4dfa77db
Tong <jeremy.tong@capgemini.com>
2018-09-25 14:34:27 -0400

    using the after functionality

--- src/main/java/com/in28minutes/spring/aop/springaop/aspect/AfterAopAspect.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/AfterAopAspect.java
@@ -1,7 +1,9 @@
 package com.in28minutes.spring.aop.springaop.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,5 +18,15 @@ public class AfterAopAspect {
 	@AfterReturning(value = "execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))", returning = "result")
 	public void afterReturning(JoinPoint joinPoint, Object result) {
 		logger.info(" {} returned with value {}", joinPoint, result);
+	}
+
+	@AfterThrowing(value = "execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))", throwing = "exception")
+	public void afterThrowing(JoinPoint joinPoint, Object exception) {
+		logger.info(" {} returned with value {}", joinPoint, exception);
+	}
+
+	@After(value = "execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
+	public void after(JoinPoint joinPoint) {
+		logger.info(" after execution of {} ", joinPoint);
 	}
 }
