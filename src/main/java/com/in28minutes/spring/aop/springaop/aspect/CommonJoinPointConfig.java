5c65faf3817141e7284acde3a62e43b04c58be7c
Tong <jeremy.tong@capgemini.com>
2018-09-25 16:05:59 -0400

    added other ways to do pointcut

--- src/main/java/com/in28minutes/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -10,4 +10,16 @@ public class CommonJoinPointConfig {
 	@Pointcut("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
 	public void businessLayerExecution() {
 	}
+
+	@Pointcut("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..)) && execution(* com.in28minutes.spring.aop.springaop.data.*.*(..))")
+	public void allLayerExecution() {
+	}
+
+	@Pointcut("bean(*dao*)")
+	public void beanIncludingDao() {
+	}
+
+	@Pointcut("within(com.in28minutes.spring.aop.springaop.data..*)")
+	public void dataLayerExecutionWithWithin() {
+	}
 }
